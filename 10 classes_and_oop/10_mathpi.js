22f38b9320ddc4a35a729a2b8b5e4f631f29ecb4
Satya7250 <satyaprakashdh3134@gamil.com>
2025-08-13 19:37:54 +0530

    advance Object in javaScript

---
+++ 10 classes_and_oop/10_mathpi.js
@@ -0,0 +1,34 @@
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+
+console.log(descripter);
+
+
+// console.log(Math.PI);
+// Math.PI = 5 //we can not change PI value
+// console.log(Math.PI);
+
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvalible: true,
+
+    orderChai: function(){
+        console.log("code fat gaya");
+        
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+// Object.defineProperty(chai, 'name', {
+//     writable: false,
+//     enumerable: false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key,value] of Object.entries(chai)){
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
